088d219716b840561b5a2b7e06d16e61c2d9332f
Willy Gauvrit <willygauvrit@Ordinateur-portable-de-Willy.local>
2026-04-28 11:18:00 +0200

    ajoute Google Tag Manager (GTM-W65L4PJD)
    Injection via next/script dans <head> + iframe noscript apres <body>.
    Strategie afterInteractive pour ne pas bloquer le rendu.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import Providers from "@/app/providers";
 import "./globals.css";
+
+const GTM_ID = "GTM-W65L4PJD";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,7 +51,28 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className="min-h-full flex flex-col bg-white text-[#0A0A0A] dark:bg-[#0A0A0A] dark:text-[#FAFAFA] transition-colors duration-300">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Providers>{children}</Providers>
       </body>
     </html>
